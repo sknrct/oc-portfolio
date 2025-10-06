f3155ae641cca471b80f1ad71f395811ed707009
sknrct <thibault.boucardgarda@gmail.com>
2025-10-06 11:16:07 +0200

    maj xp

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const Experience = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Experience</h2>
+      <h2 className="my-20 text-center text-4xl">Experiences</h2>
       {EXPERIENCES.map((experience, index) => (
         <div className="mb-8 flex flex-wrap lg:justify-center">
           <div className="w-full lg:w-1/4">
